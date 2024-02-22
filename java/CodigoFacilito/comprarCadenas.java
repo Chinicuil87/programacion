356eef1d5cede41620cd54f0489b04ae00827d9b
Chinicuil87 <tecno.ani@gmail.com>
2024-02-22 15:22:53 -0600

    mover

---
+++ java/CodigoFacilito/comprarCadenas.java
@@ -0,0 +1,16 @@
+package programacion.java.CodigoFacilito;
+
+public class comprarCadenas {
+
+    public static void main(String[] args) {
+        
+        String cadena1 = "Hola";
+        String cadena2 = "Hola";
+
+        boolean resultado = cadena1.equals(cadena2);
+
+        System.out.println(resultado);
+
+    }
+    
+}
