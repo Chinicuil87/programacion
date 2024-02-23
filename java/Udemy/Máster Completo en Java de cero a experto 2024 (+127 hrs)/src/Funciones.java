c24b950fd8291dad9ad6c8f8828a6379d284a705
Chinicuil87 <tecno.ani@gmail.com>
2024-02-23 16:16:18 -0600

    ordenar carpetas

---
+++ java/Udemy/Máster Completo en Java de cero a experto 2024 (+127 hrs)/src/Funciones.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.Scanner;
+
+public class Funciones {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("Primer numero: ");
+        var numero1 = Integer.parseInt(consola.nextLine());
+
+        System.out.println("Segundo numero: ");
+        var numero2 = Integer.parseInt(consola.nextLine());
+
+        var resultado = numero1 + numero2;
+
+        System.out.println(resultado);
+
+    }
+}
