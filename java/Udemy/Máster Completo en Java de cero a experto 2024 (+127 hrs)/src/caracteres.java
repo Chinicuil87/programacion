0bbf4f10ccb2d96d640b86126ea60bbb9bde5fc7
Chinicuil87 <tecno.ani@gmail.com>
2024-03-05 16:30:11 -0600

    actual

---
+++ java/Udemy/Máster Completo en Java de cero a experto 2024 (+127 hrs)/src/caracteres.java
@@ -0,0 +1,13 @@
+package programacion.java.Udemy.Máster
+
+import javax.swing.JOptionPane;
+
+public class caracteres {
+
+    public static void main(String[] args) {
+
+        int numero = JOptionPane.showOptionDialog(null, "Ingrese un numero entendo");
+        
+    }
+    
+}
